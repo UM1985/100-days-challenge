0728fab7e0f7e7a930899ff91a280e937211fc98
UM1985 <56utkarshpatel@gmail.com>
2025-03-06 10:10:51 +0530

    new add

---
+++ Day-76 Find Largest element in an array/code.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<limits.h>
+using namespace std;
+
+int Greatest(int arr[] , int size){
+int n = INT_MIN;
+
+for(int i =0 ; i <size ; i++){
+     if(arr[i]>n){
+        n = arr[i];
+     }
+}
+ return n;
+}
+
+int main(){
+
+    int arr[] = {3,44,665,554,6432,544,3634,66,443,345};
+
+    int size = sizeof(arr)/sizeof(arr[1]);
+
+    int greatest = Greatest(arr , size);
+
+    cout<<"Greatest number in the given array is "<<greatest;
+      
+}
